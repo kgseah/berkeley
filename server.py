703e981858e0908a2e6919f8bd3f6a5c9393d3dd
kgseah <kgseah@hotmail.com>
2020-02-07 22:09:00 +0800

    inital static page

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+PORT = 8000
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return "This is the %d visitor" % 11
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=PORT)
